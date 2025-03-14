b596252a472f79bdc6d452c546f88bb9d336bc52
tillmanjohnson <tillmanjohnson0109@gmail.com>
2025-03-14 14:54:48 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function showit(var1){
+
+document.getElementById('d1').style.display = 'none';
+document.getElementById('d2').style.display = 'none';
+document.getElementById('d3').style.display = 'none';
+document.getElementById('d4').style.display = 'none';
+
+document.getElementById(var1).style.display='block';
+}
