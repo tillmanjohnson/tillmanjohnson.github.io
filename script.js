bc744e7d071572b42e79dedbfa0e7876da094dec
tillmanjohnson <tillmanjohnson0109@gmail.com>
2025-04-02 15:17:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.getElementById('d1').style.display = 'none';
 document.getElementById('d2').style.display = 'none';
 document.getElementById('d3').style.display = 'none';
 document.getElementById('d4').style.display = 'none';
+document.getElementById('d5').style.display = 'none';
+document.getElementById('d6').style.display = 'none';
 
-document.getElementById(var1).style.display='block';
+document.getElementById(var1).style.display= 'block';
 }
