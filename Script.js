02e6e420951eb0e6f8511b37937b8830fc412e49
tillmanjohnson <tillmanjohnson0109@gmail.com>
2025-04-02 21:06:53 -0400

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,11 @@
+function showit(var1){
+
+document.getElementById('d1').style.display = 'none';
+document.getElementById('d2').style.display = 'none';
+document.getElementById('d3').style.display = 'none';
+document.getElementById('d4').style.display = 'none';
+document.getElementById('d5').style.display = 'none';
+document.getElementById('d6').style.display = 'none';
+
+document.getElementById(var1).style.display= 'block';
+}
